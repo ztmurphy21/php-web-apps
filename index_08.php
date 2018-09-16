ee7c39c4062b614443c9a62f157854a2d7442724
Zachary Murphy <ztmurphy21@gmail.com>
2018-09-16 15:06:16 -0500

    header/footer

---
+++ index_08.php
@@ -0,0 +1,8 @@
+<?php 
+include('header.html');
+?>
+<h2>Welcome to J.D Salinger Fan Club</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<?php 
+include('footer.html');
+?>

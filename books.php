da81dc500ecd2649dd45fba88b8b50d1de9ddb10
Zachary Murphy <ztmurphy21@gmail.com>
2018-09-16 15:23:09 -0500

    define function

---
+++ books.php
@@ -0,0 +1,13 @@
+<?php
+include('header.html');
+?>
+<h2>J.D. Salingers Books</h2>
+<ul>
+	<li>The Catcher in the Rye</li>
+	<li>Nine Stories</li>
+	<li>Franny and Zooey</li>
+	<li>Raise High the Roof Beam, Carpenters and Seymour: An Introduction</li>
+</ul>
+<?php
+include('footer.html');
+?>
